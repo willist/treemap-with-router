5116f2983273b1ede4b4769ed22b8fe7840b1d23
Tim Willis <tim.willis2@coxinc.com>
2016-04-28 22:39:31 -0500

    Filter out redux-tooltip noise in DevTools.

--- src/dev-tools.js
+++ src/dev-tools.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
-    <LogMonitor theme='tomorrow' />
+    <FilterMonitor blacklist={['redux-tooltip/.*']}>
+      <LogMonitor theme='tomorrow' />
+    </FilterMonitor>
   </DockMonitor>
 );
 
